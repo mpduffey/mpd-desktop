2496ad12766106aeeca97013daeb87560a3801c9
Mike Duffey <mpduffey@gmail.com>
2015-12-12 17:29:30 +0000

    Sat Dec 12 17:28:29 UTC 2015

--- mpd-desktop.js
+++ mpd-desktop.js
@@ -1,14 +1,38 @@
-angular.module('mpd.desktop', [])
+angular.module('mpd.desktop', ['ui.bootstrap',
+															 'mpd.controller',
+															 'mpd.utilities'
+															])
 .directive('mpdDesktop', function() {
 	var template = '<div class="row max-width max-height flex-item flex-container-column">\
-						<div class="row max-width">\
-							<button type="button" class="btn btn-primary" ng-click="addController()">Add Controller</button>\
-						</div>\
-						<div class="row max-width flex-item flex-container-row"></div>\
-					</div>';
+										<uib-tabset class="flex-item flex-container-column max-width">\
+											<uib-tab heading="{{tab.heading}}" active="tab.active" ng-repeat="tab in tabs" class="flex-item flex-container-column">\
+												<div class="controller-container flex-item flex-container-row" ng-controller="mpdTabCtrl">\
+													<mpd-controller ng-repeat="ctrl in ctrls"></mpd-controller>\
+												</div>\
+											</uib-tab>\
+											<uib-tab ng-click="addTab()" heading="+"></uib-tab>\
+										</uib-tabset>\
+									</div>';
 	return {
 		restrict:	'E',
 		template:	template,
-		replace:	true
+		replace:	true,
+		controller:	function($scope, $sce) {
+			$scope.tabs = [
+				{heading: "Mike", active: true},
+				{heading: "Morgan"}
+			];
+			$scope.addTab = function() {
+				$scope.tabs.push({heading: "Tab " + ($scope.tabs.length + 1), content: "add one", active: true});
+			}
+			
+			$scope.controllers = [{}, {}, {}];
+			$scope.addController = function() {
+				$scope.controllers.push({});
+			}
+		}
 	};
+})
+.controller('mpdTabCtrl', function($scope) {
+	$scope.ctrls = [{}, {}, {}];
 });
